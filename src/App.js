4618800b25112ea896747afb6983c700bcaab66f
fan <fanyoung3472@gmail.com>
2021-12-09 18:16:35 +1100

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
+import Input from './component/input';
+import './App.css';
+import CityCard from './component/cityCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header>weather App</header>
-    </div>
-  );
+
+    const apiKey="your key";
+    const [weatherData, setWeatherData]= useState([{}]);
+    const [city, setCity]= useState("");
+
+    const getWeather = (e) => {
+        if(e.key==="Enter") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`).then(
+                response => response.json()
+            ).then(
+                data => (
+                    setWeatherData(data)
+                )    
+            );
+            setCity('');
+        }
+    }
+
+    return (
+        <div className="container">
+            <Input 
+                city={city}
+                setCity={setCity}
+                getWeather={getWeather}
+            />
+            <CityCard 
+                weatherData={weatherData}
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
